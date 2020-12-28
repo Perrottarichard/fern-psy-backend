e60e8dbf304272b5c2876d422f7162b3237d5738
Richard Perrotta <perrottarichard@gmail.com>
2020-12-28 09:06:25 +0700

    add moods to login response

--- controllers/userLogin.js
+++ controllers/userLogin.js
@@ -1,33 +1,41 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-const express = require('express')
-const User = require('../models/UserSchema')
-const router = express.Router()
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const express = require("express");
+const User = require("../models/UserSchema");
+const router = express.Router();
 
 //existing user login
-router.post('/', async (request, response) => {
-  const body = request.body
-  const user = await User.findOne({ email: body.email })
+router.post("/", async (request, response) => {
+  const body = request.body;
+  const user = await User.findOne({ email: body.email });
 
-  const passwordCorrect = user === null
-    ? false
-    : await bcrypt.compare(body.password, user.passwordHash)
+  const passwordCorrect =
+    user === null
+      ? false
+      : await bcrypt.compare(body.password, user.passwordHash);
 
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
-      error: 'invalid email or password'
-    })
+      error: "invalid email or password",
+    });
   }
 
   const userForToken = {
     email: user.email,
     id: user._id,
-  }
+  };
 
-  const token = jwt.sign(userForToken, process.env.SECRET)
+  const token = jwt.sign(userForToken, process.env.SECRET);
 
-  response.status(200).send({ token, email: user.email, _id: user._id, avatarProps: user.avatarProps, avatarName: user.avatarName, heartedPosts: user.heartedPosts })
-})
+  response.status(200).send({
+    token,
+    email: user.email,
+    _id: user._id,
+    avatarProps: user.avatarProps,
+    avatarName: user.avatarName,
+    heartedPosts: user.heartedPosts,
+    moods: user.moods,
+  });
+});
 
-module.exports = router
+module.exports = router;
